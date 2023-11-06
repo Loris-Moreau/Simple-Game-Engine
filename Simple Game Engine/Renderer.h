643ecece3a9d3b7b1ae5e75fe4d55795903fa380
L M <80474693+Loris-Moreau@users.noreply.github.com>
2023-11-06 17:27:11 +0100

    maths
    p 43

--- Simple Game Engine/Renderer.h
+++ Simple Game Engine/Renderer.h
@@ -4,25 +4,40 @@
 
 #include"Rectangle.h"
 #include "Window.h"
+#include "Vector2.h"
+#include"Actor.h"
 
 class Renderer
 {
 public:
-	Renderer() = default;
+	enum class Flip //Wrapper for SDL Sprite Flipping
+	{
+		none = SDL_FLIP_NONE,
+		Horizontal = SDL_FLIP_HORIZONTAL,
+		Vertical = SDL_FLIP_VERTICAL,
+	};
 
-	inline SDL_Renderer* toSDLRenderer() const { return SDLRenderer; }
+	Renderer() = default;
+	Renderer(const Renderer&) = delete;
+	Renderer& operator=(const Renderer&) = delete;
 
 	bool initialize(Window& window);
 
 	void beginDraw();
-	void drawRect(Rectangle& rect);
+	void draw();
 	void endDraw();
 
+	void drawRect(Rectangle& rect) const; //const Correctness
+	void addSprite(class SpriteComponent* sprite);
+	void removeSprite(class SpriteComponent* sprite);
+	void drawSprites();
+	void drawSprite(const Actor& actor, const class Texture& tex, Rectangle srcRect, Vector2 origin, Flip flip)const;
+
+	SDL_Renderer* toSDLRenderer() const { return SDLRenderer; }
 	void close();
 
 private:
 	SDL_Renderer* SDLRenderer{ nullptr };
 
-	Renderer(const Renderer&) = delete;
-	Renderer& operator=(const Renderer&) = delete;
+	std::vector<SpriteComponent*> sprites;
 };
