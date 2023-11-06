efa77e9991a4d3d3575898b509563f4e4e471133
Loris M <80474693+Loris-Moreau@users.noreply.github.com>
2023-11-06 09:30:37 +0100

    [add] main file window

--- Simple Game Engine/main.cpp
+++ Simple Game Engine/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include<SDL.h>
 
 using namespace std;
 
 int main(int argc, char** args)
 {
+	//Screen Dimensions
+	constexpr int SCREEN_WIDTH = 800;
+	constexpr int SCREEN_HEIGHT = 450;
+	//Window Pointer
+	SDL_Window* window = nullptr;
 
+	//Initialize SDL
+	SDL_Init(SDL_INIT_VIDEO);
+
+	//Create Window
+	window = SDL_CreateWindow("Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+	//Wait 2 Second
+	SDL_Delay(2000);
+
+	//Destroy Window
+	SDL_DestroyWindow(window);
+
+	//Quit SDL
+	SDL_Quit;
+
+	return 0;
 }
